a3eeb973396d5a67f58ec4d699795c4101978186
solomiiariznuchok <sometimes227@gmail.com>
2016-11-15 12:49:41 +0200

    Add generateGradle custom task

---
+++ buildSrc/src/main/java/com/softserveinc/trainee/gradle/tasks/GeneratePropertyFile.java
@@ -0,0 +1,75 @@
+package com.softserveinc.trainee.gradle.tasks;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+import java.io.*;
+
+/**
+ * Created by sriznych on 15.11.2016.
+ */
+
+public class GeneratePropertyFile extends DefaultTask {
+
+    @TaskAction
+    public void setupPropertyFile() {
+        FileInputStream local = null;
+        try {
+            local = new FileInputStream("C:\\localCredentialFile.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Properties localProperties = new Properties();
+        try {
+            localProperties.load(local);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            local.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("C:\\Program Files (x86)\\Jenkins\\jobs\\mainCompile_ControlSystemOfProduct\\workspace\\service\\src\\main\\resources\\" +
+                    "database.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Properties fromGitProperties = new Properties();
+        try {
+            fromGitProperties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream("C:\\Program Files (x86)\\Jenkins\\jobs\\mainCompile_ControlSystemOfProduct\\workspace\\service\\src\\main\\resources\\" +
+                    "database.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        fromGitProperties.setProperty("password", localProperties.getProperty("password"));
+        fromGitProperties.setProperty("login", localProperties.getProperty("login"));
+        try {
+            fromGitProperties.store(out, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
