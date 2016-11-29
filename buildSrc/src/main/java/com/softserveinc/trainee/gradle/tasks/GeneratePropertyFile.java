153f568847498e5ddf96c0c578de2b8c4eef285f
solomiiariznuchok <sometimes227@gmail.com>
2016-11-29 19:08:07 +0200

    Update GeneratePropertyFile.java

--- buildSrc/src/main/java/com/softserveinc/trainee/gradle/tasks/GeneratePropertyFile.java
+++ buildSrc/src/main/java/com/softserveinc/trainee/gradle/tasks/GeneratePropertyFile.java
@@ -62,10 +62,7 @@ public class GeneratePropertyFile extends DefaultTask {
         
         fromGitProperties.setProperty("javax.persistence.jdbc.user", localProperties.getProperty("javax.persistence.jdbc.user"));
         fromGitProperties.setProperty("javax.persistence.jdbc.password", localProperties.getProperty("javax.persistence.jdbc.password"));
-        fromGitProperties.setProperty("administration.javax.persistence.jdbc.user", localProperties.getProperty("administration.javax.persistence.jdbc.user
-"));
-        fromGitProperties.setProperty("administration.javax.persistence.jdbc.password", localProperties.getProperty("administration.javax.persistence.jdbc.password
-"));
+       
         
         try {
             fromGitProperties.store(out, null);
